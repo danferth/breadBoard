9c7113818eecc05bc12641b8e7eb8fcac83c8cfa
Dan Klotz <danferth@gmail.com>
2013-05-17 17:29:54 -0700

    added some comments

--- assets/links.php
+++ assets/links.php
@@ -6,22 +6,20 @@ $bowerLocation = "assets/components";
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <?php //jQuery plugins to be loaded from boolian
 
-
+//this function checks for ture/false in plugin variables then loads plugin if needed
 function jsCheck ($jsInQuestion, $folder, $file){
-	$bowerLocation = "assets/components";
+	$bowerLocation = "assets/components";  //need to find a way to have $bowerLocation work out of scope
 	if ($jsInQuestion == false) {
 		//do nothing plugin is inactive
 	}else{
 		echo "<script src=\"".$bowerLocation."/".$folder."/".$file."\"></script>";
 	}
 }
-
+//call installed plugins
 echo jsCheck($fitText, 'FitText.js', 'jquery.fittext.js');
 echo jsCheck($validation, 'jquery.validation', 'jquery.validate.js');
 
 ?>
-
-
 
 <?php //REQUIRED CSS ?>
 <?php echo "<link rel=\"stylesheet\" href=\"".$bowerLocation."/normalize-css/normalize.css\">";?>
