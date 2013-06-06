48e81b235c8db04cca4c37caa49d3575321ee981
Dan Klotz <danferth@gmail.com>
2013-06-06 12:32:32 -0700

    used quick and dirty to start working on method of creating links from files in root minus index and template

--- content/p_quick.php
+++ content/p_quick.php
@@ -1,7 +1,29 @@
 <p>This page reserved for super <strong>Quick &amp; Dirty</strong> test of <code>php</code> or <code>js</code></p>
 <hr>
+<h2>working on creating links from files in root directory</h2>
 <?php 
 
-echo "this is working now!";
+//glob() creates aray from files ending with .php
+$fileNames = glob('*.php');
+
+//cycle through glob() array
+foreach ($fileNames as $key => $value) {
+	//remove index and template files
+	if ($value != 'index.php' && $value != 'template.php') {
+		//echo links
+		echo "<a href=\"" . $value . "\">" . $value . "</a><br>";
+	}
+}
+
+/*******************************************
+
+need to find a way to create name of link insead of using file name
+possibly naming convention can be used like "file_nameOfLink.php" and
+then use regex or some built in php function to separeate after the "_"
+and before ".php" to creat the string for the link name hmmmmmm......
+
+Time to learn regex.... 
+
+*********************************************/
 
  ?>
