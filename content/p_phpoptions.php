2e98a7c27d4088551b9071a8c8ce5a49392fdd66
Dan Klotz <danferth@gmail.com>
2013-07-01 15:12:43 -0400

    created php options page

---
+++ content/p_phpoptions.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+page content here goes into /content
+the template with add the starting <h1> under that you will need:
+
+<p>description of test</p>
+
+testing code...
+
+<h3>conclusion</h3>
+ 	     <ol>
+ 	     	<li></li>
+ 	     </ol>
+*/
+ ?>
---
+++ phpoptions.php
@@ -0,0 +1,39 @@
+<?php 
+//-------------------------------------------
+//fill in these variables for page
+//-------------------------------------------
+$title = "dynamic options in form";
+$description = "create a select form element for date options and using php to auto populate it from current year and next 7 years";
+$extraHead = "";
+//page css (if needed)
+$extraCSS = "css_phpoptions.css";
+//Extra Classes to div.taco
+$tacoExtraClasses = "";
+//main <h1>
+$heading = "<select> Dynamically Populated with php";
+//name of file for main content
+$content = "p_phpoptions.php";
+//jQuery at bottom of page
+$jsFile = "";
+
+//-------------------------------------------
+//SET OPTIONS
+//-------------------------------------------
+$mobileOption = false;
+
+//-------------------------------------------
+//SET JS PLUGINS
+//-------------------------------------------
+$fitText = false;
+$validation = false;
+$prism = false;
+
+
+
+//-------------------------------------------
+//SET TEMPLATE
+//-------------------------------------------
+
+//include template
+include 'template.php';
+?>
